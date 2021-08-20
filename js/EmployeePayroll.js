fc48458590365c7c53e0e855a3b7a46355ec0da9
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-20 17:37:44 +0530

    [Afrath]Added:- Properties profilepic,notes,department,gender to the EmployeePayrollData

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,6 +1,6 @@
 // EmployeePayroll Data Model
 class EmployeePayrollData {
-
+    // Added properties like gender,prfoile pic,notes,department
     get Empname(){
         return this._name;
     }
@@ -31,11 +31,30 @@ class EmployeePayrollData {
     set startDate(value){
         this._startDate=value;
     }
+    get Gender(){
+        return this._gender;
+    }
+    set Gender(value){
+        this._gender= value;
+    }
+    get EmployeeNotes(){
+        return this._notes;
+    }
+    set EmployeeNotes(values){
+        this._notes=value;
+        
+    }
+    get Department(){
+        return this._dept;
+    }
+    set Department(value){
+        this._dept=value;
+    } 
     toString(){
         const option = {year:'numeric', month:'long', day:'numeric'};
         const empDate = this.startDate?"undefined":this.startDate.toLocaleDateString("en-US",option);
-        return "Empname = "+this.Empname+" Profile Pic: "+this.profilePic+" Salary: "+this.salary+
-        " startDate: "+this.startDate;
+        return "Empname = "+this.Empname+" Gender: "+this.Gender+" Profile Pic: "+this.profilePic+" Department: "+this.Department+" Salary: "+this.salary+
+        " startDate: "+this.startDate+" Note: "+this.EmployeeNotes;
     }
 }
 //onSubmit validates
