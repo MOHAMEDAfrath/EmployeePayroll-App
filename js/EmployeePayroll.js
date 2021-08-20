221fdd6383a1dcb1c8d6d8b3bb2754ea1af895f1
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-20 19:51:32 +0530

    [Afrath]Added:- Validation for date

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,3 +1,38 @@
+//first loads the contents in the web page then validates
+window.addEventListener('DOMContentLoaded',(event)=>{
+    const name = document.querySelector("#name");
+    name.addEventListener('input',function(){
+        if(name.value.length == 0){
+               setTextValue('#errName',"");
+               return;
+        }
+        try{
+            (new EmployeePayrollData()).Empname = name.value;
+            setTextValue('#errName',"");
+        }catch(e){
+            setTextValue('#errName',e);
+        }
+    });
+    // salary range setter
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener("input", function () {
+      output.textContent = salary.value;
+    });
+    //validates the date
+    const date = document.querySelector('#date');
+    date.addEventListener('input',function(){
+        let startDate = getInputValue('day')+" "+getInputValue('month')+" "+getInputValue('year');
+        try{
+            (new EmployeePayrollData()).startDate = new Date(Date.parse(startDate));
+            setTextValue('.errDate',"");
+        }catch(e){
+            setTextValue('.errDate',e);
+        }
+    })
+  });
+
 // EmployeePayroll Data Model
 class EmployeePayrollData {
     // Added properties like gender,prfoile pic,notes,department
@@ -29,6 +64,11 @@ class EmployeePayrollData {
         return this._startDate;
     }
     set startDate(value){
+        if(value>new Date())
+        throw "Start Date is a future date";
+        var diff = Math.abs(new Date().getTime() - value.getTime());
+        if(diff/(1000*60*60*24)>30)
+        throw 'Start Date is Beyond 30 days';
         this._startDate=value;
     }
     get Gender(){
@@ -40,7 +80,7 @@ class EmployeePayrollData {
     get EmployeeNotes(){
         return this._notes;
     }
-    set EmployeeNotes(values){
+    set EmployeeNotes(value){
         this._notes=value;
         
     }
@@ -57,19 +97,29 @@ class EmployeePayrollData {
         " startDate: "+this.startDate+" Note: "+this.EmployeeNotes;
     }
 }
-//onSubmit validates
-const onSubmit=()=>{
+//onSubmit validates this function
+const createEmployeePayroll=()=>{
     let employee = new EmployeePayrollData();
     try{
-    employee.Empname= document.getElementById("name").value;
+    employee.Empname= getInputValue('name');
     employee.profilePic=getSelectedValues('[name = profile]').pop();
-    employee.salary = document.getElementById("salary").value;
-    employee.startDate=document.getElementById("day").value+" "+document.getElementById("month").value+" "+document.getElementById("year").value;
+    employee.salary = getInputValue("salary");
+    let date = getInputValue('day')+" "+getInputValue('month')+" "+getInputValue('year');
+   employee.Gender = getSelectedValues('[name = gender]').pop();
+  employee.Department = getSelectedValues('[name = department]');
+   employee.EmployeeNotes = getInputValue("notes");
+   
+    employee.startDate=new Date(date);
+    try{
+    employee.startDate = new Date(Date.parse(date));
+    }catch(e){
+        setTextValue('.errDate',e);
+        throw e;
+    }
     alert(employee.toString());
         
 }catch(e){
         alert(e);
-        console.log(employee.Empname);
     }
    
 };
@@ -83,6 +133,11 @@ const getSelectedValues = (values)=>{
     });
     return selectedItem;
 }
-
-
-
+const setTextValue=(id,value)=>{
+    const attribute = document.querySelector(id);
+    attribute.textContent=value;
+}
+const getInputValue = (id)=>{
+    let value = document.getElementById(id).value;
+    return value;
+}
