101d2f70301baabe588fba9ef5aa508f27789ef5
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-26 19:19:08 +0530

    [Afrath]Added:- Delete using promise

--- js/siteProperties.js
+++ js/siteProperties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local:"false",
+    use_local:"true",
     home_page:"../pages/HomePage.html",
     add_page:"../pages/Register.html",
     server_url:"http://localhost:3000/EmployeePayrollDB/"
